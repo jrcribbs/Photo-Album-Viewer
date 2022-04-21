ab4a80f83fa94cc76d94eb90e401ace83f64e3b7
JR Cribbs <jrd.cribbs@gmail.com>
2022-04-21 14:45:18 -0400

    added tests

--- test/IViewTest.java
+++ test/IViewTest.java
@@ -156,5 +156,63 @@ public class IViewTest {
             "</html>", this.webView.getSnapStrings().toString());
   }
 
+  /**
+   * Testing Webview if an extra snapshot with no extra shapes/changes from the last is passed in.
+   */
+  @Test
+  public void testBlankSnapshot() {
+    this.model.takeSnapshot("Blank");
 
+    // new webView and displaying snapshots that don't exist
+    this.webView = new WebView(model.getSnapshots(), "testOutput4.html", 1000, 1000);
+    this.webView.displaySnapshots();
+
+    assertEquals("<!DOCTYPE html>\n" +
+            "<html>\n" +
+            "<body>\n" +
+            "<h1>Your Photo Album</h1>\n" +
+            "<div>\n" +
+            "    <h2>Snapshot ID #1</h2>\n" +
+            "<div>\n" +
+            "    <h2>Description: Snapshot 1</h2>\n" +
+            "    <svg width=\"1000\" height=\"1000\">\n" +
+            "        <ellipse id=\"Oval_1\" cx=\"100.0\" cy=\"300.0\" rx=\"850.0\" ry=\"90.0\" fill=\"rgb(100,30,50)\">\n" +
+            "        </ellipse>\n" +
+            "        <rect id=\"Rectangle_1\" x=\"250.0\" y=\"600.0\" width=\"100.0\" height=\"500.0\" fill=\"rgb(0,200,1)\">\n" +
+            "        </rect>\n" +
+            "    </svg>\n" +
+            "</div>\n" +
+            "<div>\n" +
+            "    <h2>Snapshot ID #2</h2>\n" +
+            "<div>\n" +
+            "    <h2>Description: Snapshot 2</h2>\n" +
+            "    <svg width=\"1000\" height=\"1000\">\n" +
+            "        <ellipse id=\"Oval_1\" cx=\"100.0\" cy=\"300.0\" rx=\"850.0\" ry=\"90.0\" fill=\"rgb(100,30,50)\">\n" +
+            "        </ellipse>\n" +
+            "        <rect id=\"Rectangle_1\" x=\"250.0\" y=\"600.0\" width=\"100.0\" height=\"500.0\" fill=\"rgb(0,200,1)\">\n" +
+            "        </rect>\n" +
+            "        <ellipse id=\"Oval_2\" cx=\"500.0\" cy=\"500.0\" rx=\"200.0\" ry=\"200.0\" fill=\"rgb(1,255,300)\">\n" +
+            "        </ellipse>\n" +
+            "        <rect id=\"Rectangle_2\" x=\"50.0\" y=\"50.0\" width=\"50.0\" height=\"50.0\" fill=\"rgb(50,200,150)\">\n" +
+            "        </rect>\n" +
+            "    </svg>\n" +
+            "</div>\n" +
+            "<div>\n" +
+            "    <h2>Snapshot ID #3</h2>\n" + // snapshot ID 3 should match Snap ID 2
+            "<div>\n" +
+            "    <h2>Description: Blank</h2>\n" +
+            "    <svg width=\"1000\" height=\"1000\">\n" +
+            "        <ellipse id=\"Oval_1\" cx=\"100.0\" cy=\"300.0\" rx=\"850.0\" ry=\"90.0\" fill=\"rgb(100,30,50)\">\n" +
+            "        </ellipse>\n" +
+            "        <rect id=\"Rectangle_1\" x=\"250.0\" y=\"600.0\" width=\"100.0\" height=\"500.0\" fill=\"rgb(0,200,1)\">\n" +
+            "        </rect>\n" +
+            "        <ellipse id=\"Oval_2\" cx=\"500.0\" cy=\"500.0\" rx=\"200.0\" ry=\"200.0\" fill=\"rgb(1,255,300)\">\n" +
+            "        </ellipse>\n" +
+            "        <rect id=\"Rectangle_2\" x=\"50.0\" y=\"50.0\" width=\"50.0\" height=\"50.0\" fill=\"rgb(50,200,150)\">\n" +
+            "        </rect>\n" +
+            "    </svg>\n" +
+            "</div>\n" +
+            "</body>\n" +
+            "</html>", this.webView.getSnapStrings().toString());
+  }
 }
