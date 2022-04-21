27387ba180b73ffd8f3fa1a77fbf6aa49c5d2348
JR Cribbs <jrd.cribbs@gmail.com>
2022-04-21 14:52:43 -0400

    updated javadocs

--- src/controllers/IAlbumController.java
+++ src/controllers/IAlbumController.java
@@ -3,7 +3,16 @@ package controllers;
 import java.io.IOException;
 import model.IAlbumModel;
 
+/**
+ * The interface Album controller.
+ */
 public interface IAlbumController {
+  /**
+   * Runs the program using inputted model.
+   *
+   * @param model the model
+   * @throws IOException the io exception
+   */
   void go(IAlbumModel model) throws IOException;
 
 }
